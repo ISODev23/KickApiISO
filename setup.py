2e8a2d7890057d5f1f3f5a50a40fee78e8d31ffb
ISODev23 <121709677+ISODev23@users.noreply.github.com>
2025-04-13 02:07:01 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('README.md', 'r') as f:
 setup(
     name='KickApiISO',
     description='A Python package for interacting with the Kick API to retrieve channel and video data.',
-    version='0.3.2',
+    version='0.3.3',
     packages=find_packages(),
     install_requires=[
-        'requests',
+        'cloudscraper',
     ],
     license="MIT",
     keywords=['kick', 'api', 'kickapi'],
